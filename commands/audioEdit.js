cd8e4d4e86cc5caf5c1565281f68b240d7336e79
dpweb13-sys <dpweb13@gmail.com>
2025-10-17 23:16:01 +0530

    Create audioEdit.js

---
+++ commands/audioEdit.js
@@ -0,0 +1,15 @@
+module.exports = {
+    run: async (ctx) => {
+        ctx.reply(`*AUDIO-EDIT Commands:* 
+- bass
+- slow
+- blown
+- deep
+- earrape
+- fast
+- fat
+- nightcore
+- reverse
+- squirrel`);
+    }
+}
